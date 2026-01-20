228c1881f87097229d6aebb55d91431f52afd99c
John15Wil <44695179+John15Wil@users.noreply.github.com>
2026-01-20 16:21:07 +0800

    Update vite.config.ts
    修改base路径

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/anki-clone/',
+  base: '/anki-clone-joseph/',
   plugins: [
     react(),
     VitePWA({
